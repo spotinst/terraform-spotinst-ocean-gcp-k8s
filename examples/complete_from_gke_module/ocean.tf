13a24334bf8fe2bd50aca9bf7ed58957becb8049
Clint Eastwood <wesh@netapp.com>
2023-06-08 14:17:49 -0400

    cleanup uneeded code

--- examples/complete_from_gke_module/ocean.tf
+++ examples/complete_from_gke_module/ocean.tf
@@ -31,8 +31,7 @@ locals {
 
 ### Example of creating a new VNG from an existing GKE node pools via module ###
 module "ocean-gcp-k8s-vng" {
-    #source = "spotinst/ocean-gcp-k8s-vng/spotinst"
-    source = "../../../terraform-spotinst-ocean-gcp-k8s-vng"
+    source = "spotinst/ocean-gcp-k8s-vng/spotinst"
 
     for_each = toset(local.gke_node_pools_2_import)
     node_pool_name = each.key
@@ -42,21 +41,6 @@ module "ocean-gcp-k8s-vng" {
     min_instance_count  = 1
     root_volume_type    = "pd-standard"
     tags = {tag-added-after-import = ""}
-
-    
-    /*
-    network_interfaces = {
-        network = google_compute_network.vpc.name,
-        project_id = var.project_id,
-        access_configs = {
-            name = "external-nat-vng"
-            type     = "ONE_TO_ONE_NAT"
-        },
-        alias_ip_ranges = {
-            ip_cidr_range         = "/24"
-            subnetwork_range_name = google_compute_subnetwork.subnet.name
-        }
-    }*/
 
     depends_on = [module.ocean-gcp-k8s,google_container_node_pool.node-pool-for-import-1,google_container_node_pool.node-pool-for-import-2]
 }
